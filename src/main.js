6366e7f316e7ce11ee800a4e3c43eb4884b6019b
m1stD3V <jennisonernie@gmail.com>
2026-03-16 16:44:37 -0700

    Header for main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+// Name: Ernie Jennison
+// Title: Uni is Fun
+// Approx Hours: 32
+// Got assistance for precise placement of phaser drawing functions from AI, just to ensure
+// visual fidelity and quality. All systems and ideas are of my own creation.
 window.onload = function() {
     window.gradeSystem = new GradeSystem();
     const config = {
